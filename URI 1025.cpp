02c68928b6223b698c0ac785233d0bb5a412d17d
Qusai Gh <QuGhZx@gmail.com>
2016-05-27 19:48:36 +0300

    Create URI 1025.cpp

---
+++ URI 1025.cpp
@@ -0,0 +1,32 @@
+//URI Problem #1025 "Where is the Marble?"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+	int q, n, temp, kebab=0; vector <int> num;
+	cin >> n >> q;
+	while (n != 0 || q != 0) {
+		for (int i = 0; i < n; ++i) {
+			cin >> temp; num.push_back(temp);
+		}
+		sort(num.begin(), num.end());
+		cout << "CASE# " << ++kebab << ":" << endl;
+		for (int i = 0; i < q; ++i) {
+			cin >> temp;
+			int pos = find(num.begin(), num.end(), temp) - num.begin();
+			
+			if (pos==n)
+				cout << temp << " not found\n";
+			else
+				cout << temp << " found at " << pos+1 << endl;
+		}
+		num.clear(); 
+		cin >> n >> q;
+	}
+	system("pause");
+	return 0;
+}
